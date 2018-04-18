2e6c37bff86e242b6cddb9dbc17f7f1b6b50d6a2
WANZARGEN <wanjin911003@gmail.com>
2018-04-18 12:55:02 +0900

    baseuri

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -1,6 +1,11 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+var ip = require('ip');
+
+prodEnv.baseURI = '"http://' + ip.address() + ':3000"'
+prodEnv.exchangeURI = '"https://openexchangerates.org/api/"'
+prodEnv.appId = '"de0ac08850bb4c2a8eb573c120c5b74f"'
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,8 +1,11 @@
 'use strict'
-var ip = require('ip');
-module.exports = {
-  NODE_ENV: '"production"',
-  baseURI: '"http://' + ip.address() + ':3000"',
-  exchangeURI: '"https://openexchangerates.org/api/"',
-  appId: '"de0ac08850bb4c2a8eb573c120c5b74f"'
-}
+const publicIp = require('public-ip');
+publicIp.v4().then(ip => {
+  console.log(ip);
+  module.exports = {
+    NODE_ENV: '"production"',
+    baseURI: '"http://' + ip + ':3000"',
+    exchangeURI: '"https://openexchangerates.org/api/"',
+    appId: '"de0ac08850bb4c2a8eb573c120c5b74f"'
+  }
+});
